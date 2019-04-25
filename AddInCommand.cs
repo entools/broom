ee8ec8db93a91320a99514574d61551bd5a81e6d
Bogdan Mazur <b@bmazur.ru>
2019-04-25 20:57:59 +0300

    #Template
    #

--- AddInCommand.cs
+++ AddInCommand.cs
@@ -2,33 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Autodesk.Revit;
 using Autodesk.Revit.DB;
-//using System.Diagnostics;
-using System.IO;
-//using System.Collections;
 using Autodesk.Revit.UI;
-using System.Threading;
-using System.Windows;
-using Autodesk.Revit.UI.Selection;
-//using DialogResult = System.Windows.Forms.DialogResult;
-using Revit.Properties;
-using System.Globalization;
-//using Google.Apis.Auth.OAuth2;
-//using Google.Apis.Sheets.v4;
-//using Google.Apis.Sheets.v4.Data;
-//using Google.Apis.Services;
-//using Google.Apis.Util.Store;
-using Autodesk.Revit.ApplicationServices;
-using Autodesk.Revit.Attributes;
-using Autodesk.Revit.DB.Plumbing;
-using RvtOperationCanceledException = Autodesk.Revit.Exceptions.OperationCanceledException;
-//using Google.Apis.Auth;
-//using Google.Apis.Drive.v3;
-//using Google.Apis.Drive.v3.Data;
-using File = System.IO.File;
-//using System.Net;
+
 
 namespace Revit.SDK.Entools.Ribbon.CS
 {
@@ -36,106 +12,106 @@ namespace Revit.SDK.Entools.Ribbon.CS
     /// Implements the Revit add-in interface IExternalCommand,Move walls, Y direction
     /// </summary>
     /// 
-    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
-    [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
-
-    public class DelView : IExternalCommand
-    {
-
-        #region IExternalCommand Members Implementation
-        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
-                                             ref string message,
-                                             ElementSet elements)
-        {
-    //        UIApplication uiapp = revit.Application;
-    //        UIDocument uidoc = uiapp.ActiveUIDocument;
-    //        Document doc = uidoc.Document;
-
-    //        ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Views);
-    //        FilteredElementCollector collector = new FilteredElementCollector(doc);
-    //        IList<Element> list = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
-
-    //        TaskDialog.Show("m", "e.Name");
-
-    //        using (Transaction tx = new Transaction(doc))
-    //        {
-    //            tx.Start("Delete view");
-    //            foreach (Element e in list)
-    //            {
-    //                //TaskDialog.Show("m", e.Name);
-    //            }
-    //            tx.Commit();
-    //        }
-
-            return Autodesk.Revit.UI.Result.Succeeded;
-            throw new NotImplementedException();
-        }
-        #endregion IExternalCommand Members Implementation
-    }
-
-
 
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
     [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
-
-
 
     public class Broom : IExternalCommand
     {
-
-
-
-        public static Autodesk.Revit.DB.Document DbDoc;
-        private ExternalCommandData revit_;
-        
-        public void DelView()
-
+        public IList<Element> AllElnew(Document doc, BuiltInCategory name)
         {
-            TaskDialog.Show("m", "e.Name");
-            Broom.DbDoc = revit_.Application.ActiveUIDocument.Document;
-            //UIApplication uiapp = DbDoc.Application;
-            //UIApplication uiapp = revit_.Application;
-            //UIDocument uidoc = uiapp.ActiveUIDocument;
-            //Document doc = uidoc.Document;
-
-            //ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Views);
-            //FilteredElementCollector collector = new FilteredElementCollector(DbDoc);
-            //IList<Element> list = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
-
-            
-
-            //using (Transaction tx = new Transaction(DbDoc))
-            //{
-            //    tx.Start("Delete view");
-            //    foreach (Element e in list)
-            //    {
-            //        //TaskDialog.Show("m", e.Name);
-            //    }
-            //    tx.Commit();
-            //}
-            //return Autodesk.Revit.UI.Result.Succeeded;
-            throw new NotImplementedException();
+            ElementCategoryFilter filter = new ElementCategoryFilter(name);
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            IList<Element> list = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+            return list;
         }
 
 
+        public void DelView(ExternalCommandData revit)
+        {
+            UIApplication uiapp = revit.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+ 
+            List<Element> views = new List<Element>() { };
+            List<Element> delta = new List<Element>() { };
 
+            List<BuiltInCategory> cats = new List<BuiltInCategory>()
+            {
+                BuiltInCategory.OST_Views,
+                BuiltInCategory.OST_Schedules,
+                BuiltInCategory.OST_Sheets,
+            };
 
+            foreach (BuiltInCategory cat in cats)
+            {
+                IList<Element> elements = AllElnew(doc, cat);
+                views.AddRange(elements.ToList());
+            }
 
+            string names = Revit.Properties.Settings.Default["names"].ToString();
+            int i = 0;
+            bool flag = false;
 
+            string[] separators = { ",", " " };
+            string[] words = names.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (Element e in views)
+            {
+                foreach (var word in words)
+                {
+                    if (e.Name == word)
+                    {
+                        flag = true;
+                    }
+                }
+
+                if (flag == true)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    delta.Add(e);
+                }
+            }
+
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Delete view");
+                foreach (Element e in delta)
+                {
+                    try
+                    {
+                        doc.Delete(e.Id);
+                        i++;
+                    }
+                    catch// (Exception) // ex)
+                    {
+                        //TaskDialog.Show("Error", "Can not delet " + e.Name);
+                    }
+                }                
+                tx.Commit();
+            }
+
+            TaskDialog.Show("Report","Deleted " + i.ToString() + " views.");
+
+            //return Autodesk.Revit.UI.Result.Succeeded;
+            //throw new NotImplementedException();
+        }
+
+ 
         #region IExternalCommand Members Implementation
         public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
                                              ref string message,
                                              ElementSet elements)
         {
-
-
             try
             {
                 SelectionManager manager = new SelectionManager(revit);               
-                UserControl2 userControl = new UserControl2();
-                //TaskDialog.Show("*","*");
+                UserControl2 userControl = new UserControl2(revit);
+
                 return Autodesk.Revit.UI.Result.Succeeded;
             }
             catch (Exception ex)
@@ -144,8 +120,6 @@ namespace Revit.SDK.Entools.Ribbon.CS
                 message = ex.Message;
                 return Autodesk.Revit.UI.Result.Failed;
             }
-
-
             //return Autodesk.Revit.UI.Result.Succeeded;
             throw new NotImplementedException();
         }
---
+++ entoolsltbroom.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
+
+namespace Revit.SDK.Entools.Ribbon.CS
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+
+    public class EnToolsLt : IExternalApplication
+    {
+        static string AddInPath = typeof(EnToolsLt).Assembly.Location;
+        static string ButtonIconsFolder = Path.GetDirectoryName(AddInPath);
+
+        public Autodesk.Revit.UI.Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                // create customer Ribbon Items
+                RibbonPanel panel = application.CreateRibbonPanel("Broom");
+                PushButtonData list = new PushButtonData("Broom", "Broom", AddInPath, "Revit.SDK.Entools.Ribbon.CS.Broom")
+                {
+                    ToolTip = "txt"
+                };
+
+                PushButton billButton = panel.AddItem(list) as PushButton;
+
+                billButton.LargeImage = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder, "entools_img\\broom_24.png"), UriKind.Absolute));
+                billButton.Image = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder, "entools_img\\broom_20.png"), UriKind.Absolute));
+
+                //CreateRibbonEntoolsPanel(application);
+                return Autodesk.Revit.UI.Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("EnToolsLt Sample", ex.ToString());
+                return Autodesk.Revit.UI.Result.Failed;
+            }
+        }
+
+        public Autodesk.Revit.UI.Result OnShutdown(UIControlledApplication application)
+        {
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+
+
+
+
+            
+
+    }
+}
